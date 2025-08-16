bc0a4b0d41197f251944047d82dfbf0a97b23459
Tejaswini <mannemtejaswini0209@gmail.com>
2025-08-16 15:00:31 +0530

    Update q3.sql

--- q3.sql
+++ q3.sql
@@ -1,18 +1,20 @@
+DROP PROCEDURE IF EXISTS AddSubscriberIfNotExists;
 DELIMITER $$
-
 CREATE PROCEDURE AddSubscriberIfNotExists(IN subName VARCHAR(100))
 BEGIN
     DECLARE cnt INT;
+    DECLARE nextID INT;
 
     SELECT COUNT(*) INTO cnt
     FROM Subscribers
     WHERE SubscriberName = subName;
 
     IF cnt = 0 THEN
+        SELECT IFNULL(MAX(SubscriberID), 0) + 1 INTO nextID
+        FROM Subscribers;
+
         INSERT INTO Subscribers (SubscriberID, SubscriberName, SubscriptionDate)
-        VALUES ((SELECT IFNULL(MAX(SubscriberID), 0) + 1 FROM Subscribers),
-                subName, CURDATE());
+        VALUES (nextID, subName, CURDATE());
     END IF;
 END$$
-
 DELIMITER ;
